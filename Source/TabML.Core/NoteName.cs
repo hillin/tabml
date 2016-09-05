341617d4658ee65451988c257e29a05fb6f618a7
hillin <hillinsilence@gmail.com>
2016-09-05 20:23:39 +0800

    stash

--- Source/TabML.Core/NoteName.cs
+++ Source/TabML.Core/NoteName.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static TabML.Core.NoteNames;
 
 namespace TabML.Core
 {
     public struct NoteName
     {
-        public static NoteName FromSemitones(int semitones)
+        private static readonly Dictionary<int, NoteName>[] SemitoneToNoteNameLookup
+            =
+        {
+            /*0*/
+            new Dictionary<int, NoteName>
+            {
+                [7] = BSharp,
+                [0] = C,
+                [1] = new NoteName(BaseNoteName.D, Accidental.DoubleFlat)
+            },
+
+            /*1*/
+            new Dictionary<int, NoteName>
+            {
+                [0] = new NoteName(BaseNoteName.D, Accidental.DoubleSharp),
+                [1] = new NoteName(BaseNoteName.D, Accidental.DoubleFlat)
+            },
+
+        };
+
+        public static NoteName FromSemitones(int semitones, int? degreeToSnap)
         {
             
         }
