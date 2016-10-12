a6955d6a790702937b726d37f14b62d9fd67f842
hillin <hillinsilence@gmail.com>
2016-10-12 21:34:13 +0800

    stash

---
+++ Source/TabML.Editor/Tablature/Bar.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DocumentBar = TabML.Parser.Document.Bar;
+
+namespace TabML.Editor.Tablature
+{
+    public partial class Bar
+    {
+        private readonly DocumentBar _bar;
+
+        public Bar(DocumentBar bar)
+        {
+            this.InitializeComponent();
+            _bar = bar;
+        }
+    }
+}
